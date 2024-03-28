ded96663c21ecb44a6566b4bccf9417ff454834f
toastintub <johnapplebiz@gmail.com>
2024-03-27 21:05:16 -0700

    smiley face

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<h1 class="text-[300px] text-fuchsia-400 text-center font-serif">Pwn This Ass.</h1>
+<h1 class="text-[300px] text-fuchsia-400 text-center font-serif">Pwn This Ass. :]</h1>
 <p class="text-[75px] text-red-700 text-center font-serif">Forged upon the fire and flames of the Mega Crafting Table™</p>
 <p class="text-[50px] text-red-700 text-center font-serif">i keep water bottles full of your grandmothers sweat.</p>
 <p class="text-[25px] text-red-700 text-center font-serif">i have liters.</p>
