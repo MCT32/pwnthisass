d8e7b55e99c17b01f4da6584c2cf331892873a49
toastintub <johnapplebiz@gmail.com>
2024-03-27 21:02:19 -0700

    tailwind css n other such shit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Pwn This Ass.</h1>
-<p>Forged upon the fire and flames of the Mega Crafting Table™</p>
+<h1 class="text-[300px] text-fuchsia-400 text-center font-serif">Pwn This Ass.</h1>
+<p class="text-[75px] text-red-700 text-center font-serif">Forged upon the fire and flames of the Mega Crafting Table™</p>
+<p class="text-[50px] text-red-700 text-center font-serif">i keep water bottles full of your grandmothers sweat.</p>
+<p class="text-[25px] text-red-700 text-center font-serif">i have liters.</p>
